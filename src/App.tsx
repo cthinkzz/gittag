a0af3a9f3885f141330f9cb346627dc24f805dd3
CHANDRASHEKHAR PATIL <chandrashekhar.patil@heromotocorp.com>
2025-02-09 13:53:18 +0530

    0.7.1 2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React dev version 0.7.1</h1>
+      2nd commit
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
